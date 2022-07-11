701e368d1689ee8ebd557fd85c1cc7f381af7dd8
Yana <a9506862137@yandex.ru>
2022-07-11 11:34:30 +0300

    99 bottles

--- new.js
+++ new.js
@@ -1,4 +1,13 @@
-setTimeout(wakeUpUser, 5000);
-function wakeUpUser() {
-    alert("Are you going to stare at this boring page forever?");
+var word = "bottles";
+var count = 99;
+while (count > 0){
+    console.log(count + " " + word + " of beer on the wall");
+    console.log(count + " " + word + " of beer,");
+    console.log("Take one down, pass it around,");
+    count -= 1;
+    if (count > 0) {
+        console.log(count + " " + word + " of beer on the wall.");
+    } else {
+        console.log("No more " + word + " of beer on the wall.");
+    }
 }
