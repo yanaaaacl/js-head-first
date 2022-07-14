9e6a1a6eaa0097bb2c13f05a08a40ee242a9ac95
Yana <a9506862137@yandex.ru>
2022-07-14 10:45:28 +0300

    for

--- new.js
+++ new.js
@@ -1,13 +1,7 @@
-function makePhrases(){
-    var words1 = ["24/7", "multi-tier", "30,000 foot", "B-to-B", "win-win"];
-    var words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
-    var words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
-
-    var rand1 = Math.floor(Math.random() * words1.length);
-    var rand2 = Math.floor(Math.random() * words2.length);
-    var rand3 = Math.floor(Math.random() * words3.length);
-
-    var phrase = words1[rand1] + " " + words2[rand2] + " " + words3[rand3];
-    alert(phrase);
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 
+44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+var output;
+for (var i = 0; i < scores.length; i ++){
+    output = "Bubble solution #" + i + " score: " + scores[i];
+    alert(output);
 }
-makePhrases();
