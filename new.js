ad2d7905d4627966641cc7fafecdcdbe70141f42
Yana <a9506862137@yandex.ru>
2022-07-29 10:18:30 +0300

    >=

--- new.js
+++ new.js
@@ -1,6 +1,10 @@
-let shellProcced = false;
-if (shellProcced){
-    alert("You shell procced!");
-}else{
-    alert("You not shell!");
+let speedLimit = 55;
+function amISpeeding(speed){
+    if (speed >= speedLimit){
+        alert("Yes. You are speeding.");
+    }else{
+        alert("No.You are not speeding.What's wrong with you?");
+    }
 }
+amISpeeding(53);
+amISpeeding(72);
