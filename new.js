326fc9bb833a1bc4d105dd1c208ef76b2058f491
Yana <a9506862137@yandex.ru>
2022-08-01 14:22:06 +0300

    one two

--- new.js
+++ new.js
@@ -1,5 +1,12 @@
-let count = 0;
-while (count < 10){
-    document.writeln("looping away!" + "<br>");
-    count++;
-}
+
+function createPhoneNumber(numbers){
+    
+    var oneTwoThree;
+    while(oneTwoThree.length < 3 ){
+    if (numbers == 1 || numbers == 2 || numbers == 3){
+      oneTwoThree += numbers;
+    }  
+    }
+    alert(oneTwoThree);
+  }
+createPhoneNumber([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
