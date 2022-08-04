9313a6a558c2a593db02f8d485bab142d5fb214a
Yana <a9506862137@yandex.ru>
2022-08-04 10:31:28 +0300

    reduce

--- new.js
+++ new.js
@@ -1,3 +1,5 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-let evenNumbers = numbers.filter(function(item){ return (item % 2 == 0); });
-alert(evenNumbers);
+let total = numbers.reduce(function(total, current){
+    return total + current;
+}, 0);
+alert(total);
