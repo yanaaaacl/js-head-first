c90c063541a50e98a848d7b9742c58b9c71fe037
Yana <a9506862137@yandex.ru>
2022-08-04 10:21:07 +0300

    filter

--- new.js
+++ new.js
@@ -1,7 +1,3 @@
-let names = ["marge", "homer", "bart", "lisa"];
-function capitalizeItUp(item){
-    let firstLetter = item.charAt(0).toUpperCase();
-    return firstLetter + item.slice(1);
-}
-let newNames = names.map(capitalizeItUp);
-alert(newNames);
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let evenNumbers = numbers.filter(function(item){ return (item % 2 == 0); });
+alert(evenNumbers);
