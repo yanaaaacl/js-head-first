3dbd172b6adda1a3e277315c809baf6cd89d3b8b
Yana <a9506862137@yandex.ru>
2022-09-05 11:43:41 +0300

    change h1 with textContent

--- new.js
+++ new.js
@@ -1,23 +1,2 @@
-class Planet{
-    constructor(name, radius){
-        this.name = name;
-        this.radius = radius;
-    }
-    getSurfaceArea(){
-        let SurfaceArea = 4 * Math.PI * Math.pow(this.radius, 2);
-        console.log(SurfaceArea + " square km!");
-        return SurfaceArea;
-    }
-    set gravity(value){
-        console.log("Setting value!");
-        this._gravity = value;
-    }
-    get gravity(){
-        console.log("Getting value!");
-        return this._gravity;
-    }
-}
-let earth = new Planet("Earth", 6378);
-earth.gravity = 9.81;
-earth.getSurfaceArea();
-console.log(earth.gravity);
+let headingElement = document.querySelector("#bigMessage");
+headingElement.textContent = "Oppa Gangnam Style!";
