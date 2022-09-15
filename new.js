7d122ff2d240a8f1ca27073c261f026581ac95d7
Yana <a9506862137@yandex.ru>
2022-09-15 11:06:41 +0300

    factorial

--- new.js
+++ new.js
@@ -1,5 +1,8 @@
-document.addEventListener("click", changeColor, false);
-
-function changeColor(){
-    document.body.style.backgroundColor = "#ffc926";
+function factorial(n){
+    if (n == 0){
+        return 1;
+    } else{
+        return factorial(n - 1) * n;
+    }
 }
+console.log(factorial(8));
