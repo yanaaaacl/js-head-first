527ae87af26e427a263c8d979bda577ef18e2be3
Yana <a9506862137@yandex.ru>
2022-09-16 12:56:39 +0300

    else if

--- new.js
+++ new.js
@@ -1,4 +1,9 @@
-let theNumber = Number(prompt("Введите число"));
-if (!Number.isNaN(theNumber)){
-console.log("Это число является квадратным корнем из " + theNumber * theNumber);
+let num = Number(prompt("Введите число"));
+
+if (num < 10){
+    console.log("Маленькое");
+}else if(num < 100){
+    console.log("Среднее");
+}else{
+    console.log("Большое");
 }
