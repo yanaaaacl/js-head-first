b017d166ebebd6ca7dc5f9d90ddd6eedd164d7c7
Yana <a9506862137@yandex.ru>
2022-09-17 10:09:34 +0300

    #

--- new.js
+++ new.js
@@ -1,9 +1,20 @@
-let num = Number(prompt("Введите число"));
-
-if (num < 10){
-    console.log("Маленькое");
-}else if(num < 100){
-    console.log("Среднее");
-}else{
-    console.log("Большое");
+let string = "";
+counter = 0;
+while(counter < 7){
+    string += "#";
+    console.log(string);
+    counter += 1;
 }
+let newString = "";
+for(let i = 0; i < 7; i++){
+    newString += "#";
+    console.log(newString);
+}
+let newNew = "#";
+while(newNew.length <= 7){
+    console.log(newNew);
+    newNew += "#";
+    
+    
+}
+ 
