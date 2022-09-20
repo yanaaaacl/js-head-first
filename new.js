73ef5c26ef88e6768c53e9173b63e734b3e6f25e
Yana <a9506862137@yandex.ru>
2022-09-20 11:30:13 +0300

    function minimum

--- new.js
+++ new.js
@@ -1,14 +1,8 @@
-function findSolution(target){
-    function find(current, history){
-        if (current == target){
-            return history;
-        } else if (current > target){
-            return null;
-        } else {
-            return find(current + 5, `(${history} + 5)`) ||
-            find(current * 3, `(${history} * 3)`);
-        }
+function minimum (num, num2){
+    if (num > num2){
+        return num2;
+    } else{
+        return num;
     }
-    return find(1, "1");
 }
-console.log(findSolution(34));
+console.log(minimum(20, 50));
